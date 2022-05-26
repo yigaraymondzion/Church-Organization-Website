ac1b5e76a03a010f347997524f9303baf64e788f
yigaraymondzion <104869330+yigaraymondzion@users.noreply.github.com>
2022-05-26 15:09:35 +0530

    Add files via upload

---
+++ file-handler.php
@@ -0,0 +1,38 @@
+<?php
+$name = $_POST['name'];
+$visitor_email = $_POST['email'];
+$subject = $_POST['subject'];
+$message = $POST['message'];
+
+$email_from = 'info@mtzionchurch.com';
+
+$email_subject = 'New Form Submission';
+
+$email_body = "User Name: $name.\n".
+               "User Email: $visitor_email.\n".
+                "Subject: $subject.\n".
+                "User Message: $message.\n".;
+
+$to = 'mtzionchurch@gmail.com';
+
+$headers = "From: $email_from \r\n";
+
+$headers .= "Reply-To: $visitor_email \r\n";
+
+mail($to,$email_subject,$email_body,$headers);
+
+header("Location: content.html");
+
+?>
+
+
+
+
+
+
+
+
+
+
+
+?>
---
+++ js/music-list.js
@@ -0,0 +1,45 @@
+let allMusic = [
+  {
+    name: "Second Chance",
+    artist: "Apostle SsajalyaYesu",
+    img: "music-1",
+    src: "music-1"
+  },
+  {
+    name: "The Power of The House Of Jacob",
+    artist: "Pastor Kitunzi",
+    img: "music-2",
+    src: "music-2"
+  },
+  /*{
+    name: "Nobody",
+    artist: "Tim Godfrey ft IBK",
+    img: "music-3",
+    src: "music-3"
+  },
+  {
+    name: "ALl That Matters",
+    artist: "Minister GUC",
+    img: "music-4",
+    src: "music-4"
+  },
+  {
+    name: "Only You Can Satisfy",
+    artist: "William McDonald",
+    img: "music-5",
+    src: "music-5"
+  },
+  {
+    name: "Stand Amazed",
+    artist: "Sinach",
+    img: "music-6",
+    src: "music-6"
+  },
+  // like this paste it and remember to give comma after ending of this bracket }
+  // {
+  //   name: "Here is the music name",
+  //   artist: "Here is the artist name",
+  //   img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+  //   src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+  // }*/
+];
